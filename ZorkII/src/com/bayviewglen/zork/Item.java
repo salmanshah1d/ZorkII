5dbdc5a39b5c4adc909aeaa945f14714e7208734
salman <salman@10.1.141.106>
2017-05-23 15:14:02 -0400

    ok

--- ZorkII/src/com/bayviewglen/zork/Item.java
+++ ZorkII/src/com/bayviewglen/zork/Item.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.bayviewglen.zork;
 
 public class Item {
@@ -25,40 +25,4 @@ public class Item {
 		return(weight + "-pound " + description);
 	}
 }
-=======
-package com.bayviewglen.zork;
 
-public class Item {
-	private String description;
-	private int mass;
-
-	//create getters and setters
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-
-	public void setMass(int mass) {
-		this.mass = mass;
-	}
-
-	public int getMass() {
-		return mass;
-	}
-
-	public Item(String description, int mass) {
-		super();
-		this.description = description;
-		this.mass = mass;
-	}
-	public Item(String description) {
-		super();
-		this.description = description;
-		this.mass = 0;
-	}
-
-}
->>>>>>> refs/remotes/origin/master
