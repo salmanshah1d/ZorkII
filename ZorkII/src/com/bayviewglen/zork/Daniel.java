33e6e4e7d6fc169bff0d15d52a9c5518fcb9364e
jtran1 <jtran1@bayviewglen.ca>
2017-05-31 19:08:57 -0400

    Update Daniel.java
    Added buy and sell method

--- ZorkII/src/com/bayviewglen/zork/Daniel.java
+++ ZorkII/src/com/bayviewglen/zork/Daniel.java
@@ -3,7 +3,7 @@ package com.bayviewglen.zork;
 import java.util.ArrayList;
 
 public class Daniel extends NonPlayableCharacter{
-	private String characterName = "Daniel";
+	private static String characterName = "Daniel";
 	private int characterHealth = 150;
 	private int characterHealthMax = 150;
 	Weapon danielWeapon = new Weapon("Shoe ", 1.0, 10, 30, 3);
@@ -14,19 +14,22 @@ public class Daniel extends NonPlayableCharacter{
 	
 	
 	
-	public void talk(){
+	public void talk(Inventory inv){
 		boolean stillTalking = true;
 		System.out.println("Daniel: What do you want? (Select the number of the option you wish to say.)");
 		while(stillTalking){
+			System.out.println();
 		System.out.println("1: What are you doing inside a temple?");
 		System.out.println("2: What is you opinion on this game?");
 		System.out.println("3: What is you opinion on Canadian Prime Minister Justin Trudea?");
 		System.out.println("4: What is you opinion on fidget spinners?");
 		System.out.println("5: Show me your wares.");
-		System.out.println("6: Goodbye.");
-		String response  = scanner.nextLine();
-		if(response.equals("1")==false||response.equals("2")==false||response.equals("3")==false||response.equals("4")==false||response.equals("5")==false||response.equals("6")==false){
+		System.out.println("6: Take a look at what I have.");
+		System.out.println("7: Goodbye.");
+		String response  = scanner.nextLine()+"";
+		if(response.equals("1")==false&&response.equals("2")==false&&response.equals("3")==false&&response.equals("4")==false&&response.equals("5")==false&&response.equals("6")==false&&response.equals("7")==false){
 			System.out.println("That is not a valid option.");
+			System.out.println();
 		}else if(response.equals("1")){
 			System.out.println("Daniel: I was told I should go outside more. I ended up here.");
 		}else if(response.equals("2")){
@@ -37,6 +40,10 @@ public class Daniel extends NonPlayableCharacter{
 			System.out.println("Daniel: I hate them.");
 		}else if (response.equals("5")){
 			this.displayInventory();
+			this.buy(inv);;
+		}else if (response.equals("6")){
+			inv.showInventory();
+			this.sell(inv);;
 		}else{
 			System.out.println("Daniel: Cya.");
 			stillTalking = false;
@@ -45,7 +52,7 @@ public class Daniel extends NonPlayableCharacter{
 	}
 	
 	public Daniel(){
-		super();
+		super(characterName);
 		//creates the inventory for the character
 		danielInventory.add(new ArmourAttachment("Cotton Fluff(AA)", 2.0, 50, 5, "fluffy"));
 		danielInventory.add(new WeaponAttachment("Electro Magnet(WA)", 1.0,50, 12, 2, "Electro-Magnetic"));
@@ -62,11 +69,60 @@ public class Daniel extends NonPlayableCharacter{
 		System.out.println("Daniel: Heres what I have.");
 		System.out.println();
 		for (int i = 0; i<danielInventory.size(); i++){
-			System.out.print(danielInventory.get(i).getDescription()+ ": " + danielInventory.get(i).getCost() + " ");
+			System.out.print(danielInventory.get(i).getDescription()+ ": " + danielInventory.get(i).getCost() + ", ");
 		}
 	}else {
 		System.out.println("I have nothing more to sell.");
 	}
 	
+	}
+	public void buy(Inventory inv){
+		boolean stillBuying = true;
+		while(stillBuying){
+			String wishToBuy = scanner.nextLine();
+			if(wishToBuy.equals("exit"))
+				stillBuying = false;
+			else{
+				boolean isBuyable = false;
+				for (int i = 0; i<danielInventory.size(); i++){
+					if(wishToBuy.equalsIgnoreCase(danielInventory.get(i).getDescription())){
+							if(inv.getWallet()<danielInventory.get(i).getCost()){
+								System.out.println("You do not have enough to buy this item.");
+							}else{
+								inv.addItem(danielInventory.get(i));
+								System.out.println("You have bought " + danielInventory.get(i).getDescription() + ".");
+								inv.setWallet(inv.getWallet() - danielInventory.get(i).getCost());
+								System.out.println("You now have " + inv.getWallet() + "Iranian Rials.");
+								danielInventory.remove(i);
+								isBuyable = true;
+								break;	
+							}
+					}
+				}if(isBuyable == false)
+					System.out.println("That is not a valid option.");
+			}stillBuying = false;
+		}
+		
+	}public void sell(Inventory inv){
+		boolean stillSelling = true;
+		while(stillSelling){
+			String wishToSell = scanner.nextLine();
+			if(wishToSell.equals("exit"))
+				stillSelling = false;
+			else{
+				boolean isSellable= false;
+				for (int j=0; j<inv.getNumItems();j++){
+					if(wishToSell.equalsIgnoreCase(inv.getItem(j).getDescription())){
+						danielInventory.add(inv.getItem(j));
+						inv.setWallet(inv.getWallet()+inv.getItem(j).getCost());
+						inv.removeItem(inv.getItem(j));
+						System.out.println("You now have " + inv.getWallet() + "Iranian Rials.");
+						isSellable = true;
+						break;	
+					}
+				}if(isSellable== false)
+					System.out.println("That is not a valid option.");
+			}stillSelling = false;
+		}
 	}
 }
