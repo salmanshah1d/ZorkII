f4bb6dffb9fc903218846dfb6cf0dabcac13d385
salman <salman@192.168.2.104>
2017-05-31 19:23:04 -0400

    inventory and room edited to match workspace files

--- ZorkII/src/com/bayviewglen/zork/Inventory.java
+++ ZorkII/src/com/bayviewglen/zork/Inventory.java
@@ -7,9 +7,21 @@ public class Inventory {
 	ArrayList<Item> inventory;
 	private int weight = 0;
 	private double maxWeight = 50.0; // the max weight a character can make
+private int wallet = 9999999;
+	
+	public int getWallet() {
+	return wallet;
+}
+
+public void setWallet(int wallet) {
+	this.wallet = wallet;
+}
+
+
 
 	public Inventory() {
 		inventory = new ArrayList<Item>(); // construct the object
+		inventory.add(new DirtyShank());
 	}
 
 	// how to add an item to a inventory
@@ -32,7 +44,7 @@ public class Inventory {
 			System.out.println("It is too heavy to be carried!");
 		}
 	}
-
+	
 	// how to remove an item
 	public void removeItem(Item item) {
 		currentIndex = inventory.indexOf(item);
@@ -56,55 +68,67 @@ public class Inventory {
 	}
 
 	// print out the inventory you have
-	public String print() {
-		String words = "";
-		if (inventory.size() == 1) {
-			if (inventory.get(0).getDescription().charAt(0) == 'a' || inventory.get(0).getDescription().charAt(0) == 'e'
-					|| inventory.get(0).getDescription().charAt(0) == 'i'
-					|| inventory.get(0).getDescription().charAt(0) == 'o'
-					|| inventory.get(0).getDescription().charAt(0) == 'u') {
-				words += "an " + inventory.get(0).display();
-			} else {
-				words += "a " + inventory.get(0).display();
-			}
-		} else if (inventory.size() == 2) {
-			if ((inventory.get(0).getDescription().charAt(0) == 'a'
-					|| inventory.get(0).getDescription().charAt(0) == 'e'
-					|| inventory.get(0).getDescription().charAt(0) == 'i'
-					|| inventory.get(0).getDescription().charAt(0) == 'o'
-					|| inventory.get(0).getDescription().charAt(0) == 'u')
-					&& (inventory.get(1).getDescription().charAt(0) == 'a'
-							|| inventory.get(1).getDescription().charAt(0) == 'e'
-							|| inventory.get(1).getDescription().charAt(0) == 'i'
-							|| inventory.get(1).getDescription().charAt(0) == 'o'
-							|| inventory.get(1).getDescription().charAt(0) == 'u')) {
-				words += "an " + inventory.get(0).display() + " and an " + inventory.get(1).display();
-			} else if (inventory.get(0).getDescription().charAt(0) == 'a'
-					|| inventory.get(0).getDescription().charAt(0) == 'e'
-					|| inventory.get(0).getDescription().charAt(0) == 'i'
-					|| inventory.get(0).getDescription().charAt(0) == 'o'
-					|| inventory.get(0).getDescription().charAt(0) == 'u') {
-				words += "an " + inventory.get(0).display() + " and a " + inventory.get(1).display();
-			} else if (inventory.get(1).getDescription().charAt(0) == 'a'
-					|| inventory.get(1).getDescription().charAt(0) == 'e'
-					|| inventory.get(1).getDescription().charAt(0) == 'i'
-					|| inventory.get(1).getDescription().charAt(0) == 'o'
-					|| inventory.get(1).getDescription().charAt(0) == 'u') {
-				words += "a " + inventory.get(0).display() + " and an " + inventory.get(1).display();
-			} else {
-				words += "a " + inventory.get(0).display() + " and a " + inventory.get(1).display();
-			}
-		} else {
-			for (int i = 0; i < inventory.size() - 1; i++) {
-				if (inventory.get(i) != null) {
-					words += "a " + inventory.get(i).display() + ", ";
+	// print out the inventory you have
+	/*	public String print() {
+			String words = "";
+			if (inventory.size() == 1) {
+				if (inventory.get(0).getDescription().charAt(0) == 'a' || inventory.get(0).getDescription().charAt(0) == 'e'
+						|| inventory.get(0).getDescription().charAt(0) == 'i'
+						|| inventory.get(0).getDescription().charAt(0) == 'o'
+						|| inventory.get(0).getDescription().charAt(0) == 'u') {
+					words += "an " + inventory.get(0).display();
+				} else {
+					words += "a " + inventory.get(0).display();
 				}
+			} else if (inventory.size() == 2) {
+				if ((inventory.get(0).getDescription().charAt(0) == 'a'
+						|| inventory.get(0).getDescription().charAt(0) == 'e'
+						|| inventory.get(0).getDescription().charAt(0) == 'i'
+						|| inventory.get(0).getDescription().charAt(0) == 'o'
+						|| inventory.get(0).getDescription().charAt(0) == 'u')
+						&& (inventory.get(1).getDescription().charAt(0) == 'a'
+								|| inventory.get(1).getDescription().charAt(0) == 'e'
+								|| inventory.get(1).getDescription().charAt(0) == 'i'
+								|| inventory.get(1).getDescription().charAt(0) == 'o'
+								|| inventory.get(1).getDescription().charAt(0) == 'u')) {
+					words += "an " + inventory.get(0).display() + " and an " + inventory.get(1).display();
+				} else if (inventory.get(0).getDescription().charAt(0) == 'a'
+						|| inventory.get(0).getDescription().charAt(0) == 'e'
+						|| inventory.get(0).getDescription().charAt(0) == 'i'
+						|| inventory.get(0).getDescription().charAt(0) == 'o'
+						|| inventory.get(0).getDescription().charAt(0) == 'u') {
+					words += "an " + inventory.get(0).display() + " and a " + inventory.get(1).display();
+				} else if (inventory.get(1).getDescription().charAt(0) == 'a'
+						|| inventory.get(1).getDescription().charAt(0) == 'e'
+						|| inventory.get(1).getDescription().charAt(0) == 'i'
+						|| inventory.get(1).getDescription().charAt(0) == 'o'
+						|| inventory.get(1).getDescription().charAt(0) == 'u') {
+					words += "a " + inventory.get(0).display() + " and an " + inventory.get(1).display();
+				} else {
+					words += "a " + inventory.get(0).display() + " and a " + inventory.get(1).display();
+				}
+			} else {
+				for (int i = 0; i < inventory.size() - 1; i++) {
+					if (inventory.get(i) != null) {
+						words += "a " + inventory.get(i).display() + ", ";
+					}
+				}
+				words += "and " + inventory.get(inventory.size() - 1).display();
 			}
-			words += "and " + inventory.get(inventory.size() - 1).display();
+			return words;
 		}
-		return words;
-	}
 
+*/
+	public void showInventory(){
+		if(inventory.size() > 0){
+			for (int i = 0; i<inventory.size(); i++){
+				System.out.print(inventory.get(i).getDescription()+ ": " +inventory.get(i).getCost() + ", ");
+			}
+		}else {
+			System.out.println("There is nothing in your inventory.");
+		}
+		
+	}
 	// get the max weight you can carry
 	public double getMaxWeight() {
 		return maxWeight;
@@ -115,10 +139,10 @@ public class Inventory {
 	}
 
 	public Item getItem(int index) {
-		if (inventory.get(index).getDescription().equals("Sword")) {
-			return (Weapon) inventory.get(index);
+		if (inventory.get(index).getDescription().equals("Sword")){
+			return (Weapon) inventory.get(index); 
 		} else {
-			return inventory.get(index);
+		return inventory.get(index);
 		}
 	}
 }
