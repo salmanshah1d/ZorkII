d3bd431e63a8cfbd970a524b6a156e973a868007
salman <salman@10.1.141.156>
2017-05-31 13:55:23 -0400

    deleted old version which was duplicated due to merging

--- ZorkII/src/com/bayviewglen/zork/NonPlayableCharacter.java
+++ ZorkII/src/com/bayviewglen/zork/NonPlayableCharacter.java
@@ -1,37 +1,27 @@
 package com.bayviewglen.zork;
 
-<<<<<<< HEAD
-public class NonPlayableCharacter {
-	private String name;
-	
-	public NonPlayableCharacter(String nameInp){
-		this.name = nameInp;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
-=======
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NonPlayableCharacter extends Character{
-	
+public class NonPlayableCharacter extends Character {
+
 	Scanner scanner = new Scanner(System.in);
 
 	ArrayList<Item> npcInventory = new ArrayList<Item>();
-	
+
 	public ArrayList<Item> getNpcInventory() {
 		return npcInventory;
 	}
+
 	public void setNpcInventory(ArrayList<Item> npcInventory) {
 		this.npcInventory = npcInventory;
 	}
-	public NonPlayableCharacter(){
+
+	public NonPlayableCharacter() {
 		super();
-}
-	public NonPlayableCharacter(String name){
+	}
+
+	public NonPlayableCharacter(String name) {
 		super(name);
-}
->>>>>>> refs/remotes/origin/master
+	}
 }
