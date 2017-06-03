ffd65eb1ed02ec0f1e5fb7d04afd7d610e608a84
jtran1 <jtran1@bayviewglen.ca>
2017-06-02 23:27:04 -0400

    Update NonPlayableCharacter.java
    update

--- ZorkII/src/com/bayviewglen/zork/NonPlayableCharacter.java
+++ ZorkII/src/com/bayviewglen/zork/NonPlayableCharacter.java
@@ -3,25 +3,35 @@ package com.bayviewglen.zork;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NonPlayableCharacter extends Character {
-
+public class NonPlayableCharacter extends Character{
+	
 	Scanner scanner = new Scanner(System.in);
+	//private int characterPower;
 
 	ArrayList<Item> npcInventory = new ArrayList<Item>();
-
+	
 	public ArrayList<Item> getNpcInventory() {
 		return npcInventory;
 	}
-
 	public void setNpcInventory(ArrayList<Item> npcInventory) {
 		this.npcInventory = npcInventory;
 	}
-
-	public NonPlayableCharacter() {
+	public NonPlayableCharacter(){
 		super();
-	}
-
-	public NonPlayableCharacter(String name) {
+}
+	public NonPlayableCharacter(String name){
 		super(name);
+}
+/*	public int getCharacterPower() {
+		return characterPower;
+	}
+	public void setCharacterPower(int characterPower) {
+		this.characterPower = characterPower;
+	}*/
+	public void talk(Inventory inv){
+		
+	}
+	public void deathPhrase(){
+	//	System.out.println("*While dying* I see the light. It's green.");
 	}
 }
