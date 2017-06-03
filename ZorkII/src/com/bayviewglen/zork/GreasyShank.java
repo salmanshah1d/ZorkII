1771fefc33c5203e966bf94cab03746c88b3ab57
jtran1 <jtran1@bayviewglen.ca>
2017-06-02 23:22:36 -0400

    Update GreasyShank.java
    update

--- ZorkII/src/com/bayviewglen/zork/GreasyShank.java
+++ ZorkII/src/com/bayviewglen/zork/GreasyShank.java
@@ -4,7 +4,7 @@ public class GreasyShank extends Weapon{
 
 	public GreasyShank(){
 		//string, weight, cost, power, CritChance
-		super("A small blade made slick with fast food oils, and hair grease ", 5.0, 60, 30, 5);
+		super("GreasyShank", 5.0, 60, 20, 5);
 	}
 	
 }
